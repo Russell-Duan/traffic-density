26a3378d6e5f98b889a0c49588506331f74b34e9
Russell-Duan <84488454+Russell-Duan@users.noreply.github.com>
2021-12-01 07:03:35 -0800

    flask

--- app.py
+++ app.py
@@ -1,78 +1,62 @@
-'''
-In this example, we are going to look at creating, serving, and deploying a *very simple* webapp. In the following several lecture, we'll see how to add some interesting interactivity to our app. 
+from flask import Flask, render_template, request, g
 
-# Prerequisites
+import sqlite3
 
-- You need to have the flask package installed in your PIC16B Anaconda environment. 
-- You need a Heroku account.
-- You need the Heroku command line interface: 
-    - To install, at the command line (for MacOS)
-    brew tap heroku/brew && brew install heroku
-
-- At the command line, run 
-    conda activate PIC16B
-
-# Local Preview
-
-At the command line: 
-
-export FLASK_ENV=development; flask run
-
-# Deployment
-
-*Note*: these notes are written for the version of the app that Phil is using, which is indeed called pic16b-minimal-demo. In order to make your own version, you would need to give the app a different name (because one with this name already exists on the internet now).
-
-Sign up for Heroku, create app called pic16b-minimal-demo
-
-```
-heroku login
-heroku git:remote -a pic16b-minimal-demo
-
-git add *.
-git commit -m'add files for heroku'
-git push heroku
-```
-
-Then, the website is at 
-https://pic16b-minimal-demo.herokuapp.com
-
-    
-# Sources
-
-This set of lecture notes is based in part on previous materials developed by [Erin George](https://www.math.ucla.edu/~egeo/) (UCLA Mathematics) and the tutorial [here](https://stackabuse.com/deploying-a-flask-application-to-heroku/). 
-'''
-
-from flask import Flask, render_template, request
 app = Flask(__name__)
 
+def get_message_db():
+    if 'message_db' not in g: # to see if the meesage_db is in the app
+        g.message_db = sqlite3.connect("messages_db.sqlite") # connect to this database
+    g.message_db = sqlite3.connect("messages_db.sqlite")
+    cur = g.message_db.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS messages (
+    id INT(9) NOT NULL, 
+    handle VARCHAR(65535) DEFAULT NULL, 
+    message VARCHAR(65535) DEFAULT NULL,
+    PRIMARY KEY(id)
+    )''') # execute the SQL command
+    g.message_db.commit() # save the changes
+    g.message_db.close()
+    
+    return g.message_db
+
+def insert_message(request):
+    if request.method == 'POST':
+        message = request.form['message']
+        handle = request.form['handle']
+    g.message_db = sqlite3.connect("messages_db.sqlite")
+    cur = g.message_db.cursor()
+    cur.execute('''SELECT count() FROM messages''')
+    ID = cur.fetchall()[0][0]
+    cur.execute('INSERT INTO messages (ID, handle, message) VALUES (?, ?, ?)',
+                (ID, handle, message)
+            )
+    g.message_db.commit()
+    g.message_db.close()
+
+    return g.message_db
+
+def random_messages(n):
+    g.message_db = sqlite3.connect("messages_db.sqlite")
+    cur = g.message_db.cursor()
+    cur.execute('SELECT * FROM messages ORDER BY RANDOM() LIMIT '+ str(n))
+    res = cur.fetchall()
+    return res
+
 @app.route("/")
-# simplest possible approach
-# def main():
-#     return "hi there!"
 
-
-# slightly less trivial
-# def main():
-#     return render_template("main.html")
-
-# A little fancier
-
-# @app.route("/")
 def main():
-    return render_template("main_better.html")
+    return render_template("main.html")
 
 # getting basic user data
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        g.message_db = get_message_db()
+        insert_message(request)
+    return render_template("submit.html")
 
-# 
-@app.route('/profile/<name>/')
-def hello_name(name):
-    return render_template('profile.html', name=name)
+@app.route('/view/', methods=['POST', 'GET'])
+def view():
+    res = random_messages(3)
+    return render_template("view.html", res = res)
